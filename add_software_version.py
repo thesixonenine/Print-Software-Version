72d6a763f121e1a1dbef835de00f796fe26ecb5e
Simple <huyang1994822@gmail.com>
2023-08-16 11:48:02 +0800

    Update add_software_version.py

--- add_software_version.py
+++ add_software_version.py
@@ -11,8 +11,8 @@ def main():
 
 if __name__ == "__main__":
     main()
-    os.system('git config --global user.name ' + os.getenv('GIT_NAME'))
-    os.system('git config --global user.email ' + os.getenv('GIT_EMAIL'))
+    os.system('git config user.name ' + os.getenv('GIT_NAME'))
+    os.system('git config user.email ' + os.getenv('GIT_EMAIL'))
     os.system('git add software')
     os.system('git commit -am \'add software version\'')
     os.system('git push')
